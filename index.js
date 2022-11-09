dfc9c535df9a94fdb9dc91c3bdf309a3f8fc8cc5
apisceva <aljona.gromova@gmail.com>
2022-11-09 08:28:44 -0500

    Lead tracker

---
+++ index.js
@@ -0,0 +1,24 @@
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  renderLeads();
+});
+
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'> 
+                    ${myLeads[i]}
+                </a> 
+            </li> 
+        `; //makes link clickable and opens in a new tab
+  }
+  ulEl.innerHTML = listItems;
+}
